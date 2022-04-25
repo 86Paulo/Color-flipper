32d9080ef5b8eed40d2ee6bfb699d7759e26ea53
Paulo Araújo <90206752+86Paulo@users.noreply.github.com>
2022-04-25 21:18:00 +0100

    app.js done
    simple color flipper done

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function() {
+    // get random number between 0 - 3 from the colors arr
+    // const randomNumber = 2;
+    const randomNumber = getRandom();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandom() {
+    return Math.floor(Math.random() * colors.length);
+}
